b8fe3a2144e8d35e120da197c75c368ce7625868
David Pashley <david@codingcraft.co.uk>
2018-09-21 13:29:39 +0200

    Implement StepOne.run()
    This implements StepOne.run() such that the unit test passes correctly.
    
    This uses IntStream.rangeClosed() as basic for loops are a source of
    off-by-one errors, and this is more explicit about what it's doing.
    
    There is the obvious temptation to implement this as a map operation
    followed by a .collect(joining(" ")), but this will require keeping the
    entire output in memory before sending it to the output. (We do copy the
    output to memory for testing, using a ByteArrayOutputStream wrapped in a
    PrintWriter.)

--- src/main/java/com/davidpashley/upwork/StepOne.java
+++ src/main/java/com/davidpashley/upwork/StepOne.java
@@ -1,22 +1,50 @@
 package com.davidpashley.upwork;
 
 import java.io.PrintStream;
+import java.util.stream.IntStream;
 
 public class StepOne {
 
-    public void run(int max, PrintStream out) {
+    /* This method takes a PrintStream as an input so that we can grab the
+     * output in unit tests. The public run(int) method uses System.out as a
+     * default.
+     */
+    void run(int max, PrintStream out) {
+        IntStream.rangeClosed(1, max)
+                .forEach(i -> {
+                    if (i % 15 == 0) {
+                        out.print("fizzbuzz");
+                    } else if (i % 5 == 0) {
+                        out.print("buzz");
+                    } else if (i % 3 == 0) {
+                        out.print("fizz");
+                    } else {
+                        out.print(i);
+                    }
+                    if (i != max) {
+                        out.print(" ");
+                    }
+                });
 
     }
 
+    /**
+     * Output to standard out a list of numbers from 1 to @{code max}, transforming
+     * the number using the following rules:
+     *
+     * * If the number is divisible by three, replace it with 'fizz'
+     * * If the number is divisible by five, replace it with 'buzz'
+     * * If the number is divisible by three and five, replace it with 'fizzbuzz'
+     * @param max the maximum number to output
+     */
     public void run(int max) {
         run(max, System.out);
     }
 
-    public int main(String... args) {
+    public static void main(String... args) {
         int max = args.length > 0 ? Integer.valueOf(args[0]) : 20;
 
         var stepOne = new StepOne();
         stepOne.run(max);
-        return 0;
     }
 }
